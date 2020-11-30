cf2e5b582a10c656680d66b6550739026e253562
Biel <llinasbb1@campusemav.com>
2020-11-30 10:29:39 +0100

    Laberint 30/11/2020
    Laberint 30/11/2020

---
+++ Assets/Scripts/ScrPickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrPickUp : MonoBehaviour
+{
+    [SerializeField]
+    int velGir = -90;
+
+    public int valor = 1;
+
+    // Start is called before the first frame update
+  
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, velGir * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/ScrPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrPlayer : MonoBehaviour
+{
+    [SerializeField]
+    float forsa = 3;
+    float x, y;  // variables per guardar lectura dels cursors
+
+    // Per accedir al component RigidBody:
+    Rigidbody2D rb;
+    ScrPickUp scrP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // ara rb apunta al component rigidBody del player
+        // print("En aquesta escena: " + ScrControlGame.pickups);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");  // llegim moviment horitzontal
+        y = Input.GetAxis("Vertical");    // llegim moviment vertical
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(new Vector2(x * forsa, y * forsa));
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // print(collision.name);
+        // if (collision.tag == "pickup") Destroy(collision.gameObject);
+       
+        
+    }
+
+}
